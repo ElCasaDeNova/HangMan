39fe3c172b2a7541a87143a23bce1b9bad725931
Théophile <theoyoupy@orange.fr>
2024-09-27 12:48:23 +0200

    Upgrade TerrainPooler
    Use a List of Active Child

--- Assets/Scripts/Util/TerrainCollisionDetector.cs
+++ Assets/Scripts/Util/TerrainCollisionDetector.cs
@@ -1,52 +1,56 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class TerrainCollisionDetector : MonoBehaviour
 {
     [SerializeField]
     private TerrainPooler pooler;
-    [SerializeField]
-    private GameObject previousTerrain;
-    [SerializeField]
-    private GameObject currentTerrain;
-    [SerializeField]
-    private GameObject nextTerrain;
-    [SerializeField]
-    private float terrainLength = 21f;
+
+    private List<GameObject> activeTerrains;
+
+    private float terrainLength = 50f;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("test");
+            activeTerrains = GetListOfTerrain();
 
-            // Deactivate previous terrain and return it to the pool
-            if (previousTerrain != null)
+            // Check if the list contains at least one terrain to remove
+            if (activeTerrains.Count > 0)
             {
-                pooler.ReturnTerrainToPool(previousTerrain);
+                // Get and remove the first terrain in the list (the oldest one)
+                GameObject firstTerrain = activeTerrains[0]; // Head of the list
+                pooler.ReturnTerrainToPool(firstTerrain);
+                activeTerrains.RemoveAt(0); // Remove it from the list
             }
 
-            // Reorganize terrain order
-            previousTerrain = currentTerrain; // Update previous terrain
-            currentTerrain = nextTerrain; // Update current terrain
-
-            // Generate the next terrain
-            GameObject pooledTerrain = pooler.GetNextTerrain(); // Get the new terrain
-
-            // Check if nextTerrain is valid
+            // Generate the next terrain and add it to the end of the list
+            GameObject pooledTerrain = pooler.GetNextTerrain();
             if (pooledTerrain != null)
             {
-                pooledTerrain.SetActive(true); // Activate the new terrain
+                pooledTerrain.SetActive(true);
 
-                // Position the new terrain correctly
-                pooledTerrain.transform.position = nextTerrain.transform.position + new Vector3(terrainLength, 0, 0);
+                // Position the new terrain at the end of the last terrain in the list
+                if (activeTerrains.Count > 0)
+                {
+                    // Position the terrain
+                    GameObject lastTerrain = activeTerrains[activeTerrains.Count - 1]; // Last terrain
+                    pooledTerrain.transform.position = this.transform.position + new Vector3(terrainLength, 0, 0);
+                }
+                else
+                {
+                    // If the list is empty, position the terrain at the origin
+                    pooledTerrain.transform.position = Vector3.zero;
+                }
 
-                // Update the terrain references in the new terrain
+                activeTerrains.Add(pooledTerrain); // Add the new terrain to the end of the list
+
+                // Assign the same pooler and list to the new terrain detector
                 TerrainCollisionDetector newTerrainDetector = pooledTerrain.GetComponent<TerrainCollisionDetector>();
                 if (newTerrainDetector != null)
                 {
                     newTerrainDetector.pooler = pooler;
-                    newTerrainDetector.previousTerrain = previousTerrain;
-                    newTerrainDetector.currentTerrain = currentTerrain;
                 }
             }
             else
@@ -54,5 +58,21 @@ public class TerrainCollisionDetector : MonoBehaviour
                 Debug.LogWarning("Next terrain is null, check TerrainPooler.");
             }
         }
+    }
+
+    private List<GameObject> GetListOfTerrain()
+    {
+        GameObject parent = gameObject.transform.parent.gameObject;
+        List<GameObject> listOfTerrains = new List<GameObject>();
+
+        foreach (Transform terrain in parent.transform)
+        {
+            if (terrain.gameObject.activeInHierarchy)
+            {
+                listOfTerrains.Add(terrain.gameObject);
+            }
+        }
+
+        return listOfTerrains;
     }
 }
