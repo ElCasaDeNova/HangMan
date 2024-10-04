d2ae821206494c3bb5af81c030af236399a909e2
Théophile <theoyoupy@orange.fr>
2024-10-04 15:26:03 +0200

    Add Sound to Jump Animation

---
+++ Assets/Animation/Walking/Jump/Jump.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    private AudioSource audioSource;
+    [SerializeField] AudioClip jumpStart;
+    [SerializeField] AudioClip jumpEnd;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void JumpStart()
+    {
+        audioSource.PlayOneShot(jumpStart);
+    }
+
+    void JumpEnd()
+    {
+        audioSource.PlayOneShot(jumpEnd);
+    }
+}
